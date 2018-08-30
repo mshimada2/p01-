71c16d82233c657ca279c27dcc180e8f946fe76e
mshimada <mshimada2@mail.csuchico.edu>
2018-08-30 14:23:07 -0700

    gonna test for shits and gigs

---
+++ chart.cpp
@@ -0,0 +1,79 @@
+//Author: McDermott Liam Shimada 
+
+#include<iostream>
+
+
+using namespace std;
+
+
+//prototypes
+
+void printBars();
+int findLargest(int, int);
+
+
+
+int main()
+{
+  
+  const int MAX = 100; 
+  int numberSet[MAX];
+  int largest = findLargest(values,size);
+
+
+  cout<<"Please enter numbers. You can go up to 100. Input 0 to quit "<<endl;
+
+ 
+  while(numberSet[MAX] != 0 || numberSet[MAX] == MAX)
+  {
+
+  cin>>numberSet[MAX];
+
+  }
+
+  if(numberSet[MAX] == 0)
+  {
+    printBars.numberSet[MAX];
+
+  }
+
+
+
+  return 0;
+
+}
+
+//functions
+
+//returns the largest number in the given array of numbers
+
+int largestNumber(int values[], int size)
+{
+  int largest = 0; 
+
+  if(
+
+
+  return largest; 
+
+//prints out the astericks graph bars
+void printBars(int numbers)
+{
+  for(x = 0; x < numbers.size(); x++)
+  {
+    cout<<numbers<<" ";
+  }
+
+}
+
+/*returns the largest value given an array of integers
+int findLargest(int numberSet[MAX], int size)
+{
+  int largest = 0;
+
+  return largest;
+
+
+
+}
+*/

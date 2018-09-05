5b06ec1323c8e02b84673c367c7499440d9f3b5d
mshimada <mshimada2@mail.csuchico.edu>
2018-09-04 21:02:00 -0700

    almsot final

--- chart.cpp
+++ chart.cpp
@@ -36,12 +36,13 @@ int main()
   //calls find_largest function by assigning it to maxHeight 
   maxHeight = find_largest(values, index);
 
-  //iterates through to see how high the max height needs to print (x-- is key to making it print out the way you want)
+  //iterates through to see how high the max height needs to print (x-- is key to making it print the height like its going  bottom to top
   for(int x = maxHeight; x > 0; x--)
   {
     //prints out the width 
     for(int y = 0; y < maxWidth; y++)
     {
+      //prints astericks if values[y] of width is greater than or equal to x or height; anything ELSE then print space
       if(values[y] >= x)
       {
         cout<<'*';
@@ -65,9 +66,10 @@ int main()
 int find_largest(int values[], int size)
 {
 
+  //declare largest variable after the FOR loop it can hold the largest number 
   int largest = 0;
-//imperative to have this for loop. it compares the next entered number with the last to find the greatest number and
-//returns it
+//imperative to have this FOR loop. it compares the next entered number with the last entered number to find the greatest 
+//number and returns it so the program knows how high to print for the height, 
   for(int x = 0; x < size; x++)
   {
     if(values[x] > largest)
